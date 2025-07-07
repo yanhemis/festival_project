291b8fe6a04e1b92b4e13119f6f20b83e614dbfe
gamja <bear356se@gmail.com>
2025-07-08 03:11:36 +0900

    Update main.dart

--- festivalApp/lib/main.dart
+++ festivalApp/lib/main.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:festival_app/screens/login_screen.dart';
-// Amplify 관련 import 추가
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'amplifyconfiguration.dart';  // amplify init 시 자동 생성된 파일
+import 'amplifyconfiguration.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Amplify 설정을 추가하고 앱을 실행합니다.
   await _configureAmplify();
   runApp(const MyApp());
 }
 
 Future<void> _configureAmplify() async {
   try {
-    // 추가할 플러그인 목록을 만듭니다.
     final auth = AmplifyAuthCognito();
     await Amplify.addPlugins([auth]);
 
-    // amplifyconfiguration.dart 파일의 내용을 사용하여 Amplify를 구성합니다.
     await Amplify.configure(amplifyconfig);
   } on Exception catch (e) {
     print('Could not configure Amplify: $e');
