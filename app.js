1069da5965fc52d278ad3b6ef1b8c3e5f1b81c94
lepidia <144655930+lepidia@users.noreply.github.com>
2025-07-17 12:51:35 +0900

    Update app.js
    Access 토큰 보호 미들웨어 생성

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
 
+const verifyToken = require('./middleware/auth');
+
+app.get('/protected', verifyToken, (req, res) => {
+  res.json({ message: `Hello ${req.user.userId}! This is protected.` });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
