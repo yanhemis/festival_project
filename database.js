71bda0ecdf7cc225eb63eff68ff448a17ca3a385
lepidia <144655930+lepidia@users.noreply.github.com>
2025-08-14 13:51:38 +0900

    Create database.js

---
+++ database.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host : env.process.DB_HOST,
+        dialect : "mysql",
+        port : process.env.DB_PORT
+    }
+);
+
+module.exports = sequelize;
