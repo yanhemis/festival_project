d7fcd69e884f013a6c0d43ae93eecd468d11deb7
lepidia <144655930+lepidia@users.noreply.github.com>
2025-08-14 14:04:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -64,5 +64,5 @@ app.delete('/posts/:id', async (req, res) => {
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`🚀 서버 실행 중: http://localhost:${port}`);
+    console.log(`서버 실행 중: http://localhost:${port}`);
 });
