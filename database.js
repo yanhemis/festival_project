2c47e4da6d0a818a25b981e5e190300757f8a712
yanhemis <87313463+yanhemis@users.noreply.github.com>
2025-08-16 23:45:59 +0900

    Update database.js
    Check Error

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        ##process.env.DB_HOS 가 아니라 env.process.DB_HOST가 맞는지 확인##
+        ##process.env.DB_HOST 가 아니라 env.process.DB_HOST가 맞는지 확인##
         host : env.process.DB_HOST,
         dialect : "mysql",
         port : process.env.DB_PORT
