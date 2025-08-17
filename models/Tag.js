4591f508a8dee8a8c003e2d339a859f8a61d95e2
yanhemis <87313463+yanhemis@users.noreply.github.com>
2025-08-17 14:54:38 +0900

    Update Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,11 +1,18 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../database");
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
 
-const Tag = sequelize.define("Tag", {
-  name: {
-    type: DataTypes.STRING,
-    unique: true
+const Tag = sequelize.define('Tag',  {
+
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
   }
-});
-
-module.exports = Tag;
+};
