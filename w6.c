e4411198b7cd85f2086f43794ed60aefab7f3816
lepidia <144655930+lepidia@users.noreply.github.com>
2025-10-12 11:20:14 +0900

    Create w6.c

---
+++ w6.c
@@ -0,0 +1,72 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+pid_t child_pid;
+int total_signals;
+int sent = 0;
+int acked = 0;
+int received = 0;
+
+void receiver_handler(int sig) {
+    if (sig == SIGUSR1) {
+        received++;
+        printf("receiver: received signal #%d and sending ack\n", received);
+        kill(getppid(), SIGUSR1); 
+    } 
+    else if (sig == SIGINT) {
+        printf("receiver: received %d signals\n", received);
+        _exit(0);
+    }
+}
+
+void recv_ack_handler(int sig) {
+    acked++;
+}
+
+void sending_handler(int sig) {
+    if (acked >= total_signals) {
+        printf("all signals have been sent!\n");
+        kill(child_pid, SIGINT);
+        exit(0);
+    } else {
+        printf("sender: total remaining signal(s): %d\n", total_signals - acked);
+        kill(child_pid, SIGUSR1);
+        sent++;
+    }
+    alarm(1); 
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <number_of_signals>\n", argv[0]);
+        exit(1);
+    }
+
+    total_signals = atoi(argv[1]);
+    if (total_signals <= 0) {
+        fprintf(stderr, "Invalid number of signals\n");
+        exit(1);
+    }
+
+    child_pid = fork();
+
+    if (child_pid == 0) {
+        signal(SIGUSR1, receiver_handler);
+        signal(SIGINT, receiver_handler);
+        while (1) pause();
+    } else {
+        signal(SIGUSR1, recv_ack_handler);
+        signal(SIGALRM, sending_handler);
+
+        kill(child_pid, SIGUSR1);
+        sent++;
+        alarm(1);
+
+        while (1) pause();
+    }
+
+    return 0;
+}
